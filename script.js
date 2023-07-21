982b10eace5812277301e81b05692fc829fcd466
xan <xansaif162@gmail.com>
2023-07-22 03:29:34 +0500

    Make rgb without randomness

--- script.js
+++ script.js
@@ -5,49 +5,50 @@ let btn = document.getElementById("button")
 lineDiv.classList.add("line");
 inlineDiv.classList.add("squre")
 
-function creatGrid(num)
+function creatGrid(num,color)
 {
     container.innerHTML = "";
 for (let i = 1; i<=num; i++)
 {
-    
     let lineDiv = document.createElement("div");
     lineDiv.classList.add("line");
     container.appendChild(lineDiv);
     console.log("line")
     for (let i=1;i<=num;i++)
     {   
-        
+        let userColor = color;
         let inlineDiv = document.createElement("div");
         inlineDiv.classList.add("squre");
-
-        inlineDiv.addEventListener("mouseover",() => inlineDiv.classList.add("hover"));
         //inlineDiv.textContent = "a";
+        inlineDiv.addEventListener("mouseover",() => inlineDiv.style.cssText = `background-color: ${userColor};`);
         lineDiv.appendChild(inlineDiv);
         console.log("inline")
     }    
 }
 }
 
+
+
 //let grid_size = alert("Enter gride size");
-creatGrid(16)
+creatGrid(16,"black")
 
 function setGriidSize ()
 {
-
     let userInput = prompt("Enter the grid size");
     if(userInput>100)
     {
         alert("too high enter number lower then 100");
     }
     
-    else 
-    {
-    creatGrid(userInput);
-    container.removeChild(btn);
-    }
+    else creatGrid(userInput,randomColor());
 }
 
+function randomColor ()
+{
+    colors = ["red","green","blue"]
+    let userColor = colors[Math.floor(Math.random()*3)];
+    return userColor;
+}
 btn.addEventListener("click",() => setGriidSize());
 
 //let squre = document.querySelector(".squre");
